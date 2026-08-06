ca165f42ff9126cb25a14b01362362127c0687c7
Sakshi parmar <sakshiparmar2828@gmail.com>
2026-08-06 16:04:35 +0530

    Production changes

--- server/src/index.js
+++ server/src/index.js
@@ -2,20 +2,32 @@ import express from "express";
 import dotenv from "dotenv";
 import authRouter from "./routes/auth.route.js";
 import messageRouter from "./routes/message.route.js";
+import path from "path";
 
 const PORT = process.env.PORT || 8000;
 const app = express();
 app.use(express.json())
 
+const __dirname = path.resolve();
 
 dotenv.config();
 
-app.get("/", (req, res) => {
-  res.send("Hello from server");
-}
-)
+// app.get("/", (req, res) => {
+//   res.send("Hello from server");
+// }
+// )
 app.use("/api/messages", messageRouter);
 app.use("/api/auth", authRouter);
+
+console.log("NODE_ENV:", process.env.NODE_ENV);
+
+if(process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/dist")));
+  app.get("*", (_ , res) => {
+    res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+  }
+  )
+}
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
