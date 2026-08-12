78bb21cfd38faa1790de799ea528899675fc782c
Sakshi Parmar <140688243+sakshi-parmar2004@users.noreply.github.com>
2026-08-12 13:39:12 +0530

    Remove root route handler
    Removed the root route handler that responded with 'Server is running'.

--- server/src/index.js
+++ server/src/index.js
@@ -23,10 +23,7 @@ const __dirname = path.resolve();
 
 
 
-app.get("/", (req, res) => {
-  res.send("Server is running");
-}
-);
+
 app.use("/api/messages", messageRouter);
 app.use("/api/auth", authRouter);
 
